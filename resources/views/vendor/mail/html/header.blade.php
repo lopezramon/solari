90aeb82b2b635ca7e16af9aa03842bd143935dcd
Carla Ramirez <carla.ramirez.rojas@gmail.com>
2018-09-25 18:36:08 -0400

    merge develop en booking

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,8 @@
 <tr>
     <td class="header" style="background-color: #000; padding: 0px;">
         <a href="{{ $url }}">
-           <img src="{{ asset('images/home/logo_footer.svg') }}" style="-ms-interpolation-mode:bicubic;clear:both;display:block;max-width:80%;outline:0;text-decoration:none;width:50%;margin: auto;">
+           <img src="{{ asset('/images/logos/logo2-medium.png') }}" style="-ms-interpolation-mode:bicubic;clear:both;display:block;max-width:80%;outline:0;text-decoration:none;width:80px;margin: auto;">
         </a>
+        <h2 class="text-center" style="Margin:0;Margin-bottom:10px;color:#fff;font-family: 'Nunito', sans-serif;font-size:30px;font-weight:400;line-height:1.3;margin:0;margin-bottom:10px;padding:0;text-align:center;word-wrap:normal">Luxury Room</h2>
     </td>
 </tr>
