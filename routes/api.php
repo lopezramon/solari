3334a45fe4585d4fc3df820dcc52988228e51b18
Steven Trabajo Jumrr <steven.g.s.p@gmail.com>
2018-09-26 14:19:24 -0400

    ocultar booking API

--- routes/api.php
+++ routes/api.php
@@ -123,6 +123,7 @@ Route::get('admin/rooms/{rooms}', 'API\Admin\RoomAPIController@show');
 Route::get('admin/bookings/user/{user_id}', 'API\Admin\BookingAPIController@index');
 Route::post('admin/bookings', 'API\Admin\BookingAPIController@store');
 Route::get('admin/bookings/{bookings}', 'API\Admin\BookingAPIController@show');
+Route::put('admin/bookings/hide/{bookings}', 'API\Admin\BookingAPIController@hide');
 // Route::put('admin/bookings/{bookings}', 'API\Admin\BookingAPIController@update');
 // Route::patch('admin/bookings/{bookings}', 'API\Admin\BookingAPIController@update');
 // Route::delete('admin/bookings/{bookings}', 'API\Admin\BookingAPIController@destroy');
