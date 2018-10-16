1b4678c8ab734b1053cd8c78a31920ad411092b5
richard hernandez <richard.hernandez@jumperr.com>
2018-10-16 13:51:38 -0400

    cambios room

--- resources/assets/js/components/booking/rooms.vue
+++ resources/assets/js/components/booking/rooms.vue
@@ -2,7 +2,10 @@
     <div>
         <article v-for="room in rooms" class="mb-5 border-bottom">
             <div class="container-fluid">
-                <div class="row">
+                <div class="row position-relative ">
+                    <div class="ribbon ribbon-top-right">        
+                        <span>Disponible</span>
+                    </div>
                     <div class="col-12">
                         <h2 v-text="room.name"></h2>
                         <h3 v-text="room.subtitle1"></h3>
@@ -10,7 +13,7 @@
                     </div>
                     <!-- slider -->
                     <div class="col-12 col-lg-8 mb-3">
-                        <swiper :options="swiperOption">
+                        <swiper :options="swiperOption ">
                             <swiper-slide v-for="(slide, index) in room.galery" :key="index">
                                 <img class="img-fluid" :src="slide.image" alt="">
                             </swiper-slide>
@@ -111,3 +114,79 @@ export default {
     }
 }
 </script>
+
+
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Lato:700);
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #f0f0f0;
+}
+.box {
+  position: relative;
+  max-width: 600px;
+  width: 90%;
+  height: 400px;
+  background: #fff;
+  box-shadow: 0 0 15px rgba(0,0,0,.1);
+}
+
+/* common */
+.ribbon {
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+  position: absolute;
+}
+.ribbon::before,
+.ribbon::after {
+  position: absolute;
+  z-index: -1;
+  content: '';
+  display: block;
+  border: 5px solid #2980b9;
+}
+.ribbon span {
+  position: absolute;
+  display: block;
+  width: 225px;
+  padding: 15px 0;
+  background-color: #3498db;
+  box-shadow: 0 5px 10px rgba(0,0,0,.1);
+  color: #fff;
+  font: 700 18px/1;
+  text-shadow: 0 1px 1px rgba(0,0,0,.2);
+  text-transform: uppercase;
+  text-align: center;
+}
+
+/* top right*/
+.ribbon-top-right {
+  top: -10px;
+  right: -10px;
+}
+.ribbon-top-right::before,
+.ribbon-top-right::after {
+  border-top-color: transparent;
+  border-right-color: transparent;
+}
+.ribbon-top-right::before {
+  top: 0;
+  left: 0;
+}
+.ribbon-top-right::after {
+  bottom: 0;
+  right: 0;
+}
+.ribbon-top-right span {
+  left: -25px;
+  top: 30px;
+  transform: rotate(45deg);
+}
+
+
+</style>
